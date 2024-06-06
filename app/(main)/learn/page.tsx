f30f956bd83f5138839f74e28036134a214e1bf8
Roukaya Chelly <roukayachelly7@gmail.com>
2024-06-06 15:42:14 +0100

    updates

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,22 @@
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
 const LearnPage = () => {
-    return (
-        <div>
-            <h1>Learn Page</h1>
-        </div>
-    );
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        ></UserProgress>
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Spanish" />
+      </FeedWrapper>
+    </div>
+  );
 };
 export default LearnPage;
