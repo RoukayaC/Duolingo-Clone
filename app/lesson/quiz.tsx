e03a1492458d2d2908b10edef23970d25f8d7a39
Roukaya Chelly <roukayachelly7@gmail.com>
2024-06-08 16:43:51 +0100

    add challenge footer

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
+
 type Props = {
   initialPercentage: number;
   initialHearts: number;
@@ -32,8 +34,16 @@ export const Quiz = ({
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
+  const [selectedOption, setSelectedOption] = useState<number>();
+  const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
+
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onSelect = (id: number) => {
+    if (status !== "none") return;
+    setSelectedOption(id);
+  };
 
   const title =
     challenge.type === "ASSIST"
@@ -60,9 +70,9 @@ export const Quiz = ({
               )}
               <Challenge
                 options={options}
-                onSelect={() => {}}
-                status="none"
-                selectedOption={undefined}
+                onSelect={onSelect}
+                status={status}
+                selectedOption={selectedOption}
                 disabled={false}
                 type={challenge.type}
               />
@@ -70,6 +80,10 @@ export const Quiz = ({
           </div>
         </div>
       </div>
+      <Footer 
+      disabled={!selectedOption} 
+      status={status} 
+      onCheck={() => {}} />
     </>
   );
 };
